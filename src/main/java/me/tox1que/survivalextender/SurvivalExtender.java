521479f74a0f00c4c5d45317af35ae5189974103
Robert <robikvosika@seznam.cz>
2024-01-10 20:45:53 +0100

    enhance main class

--- src/main/java/me/tox1que/survivalextender/SurvivalExtender.java
+++ src/main/java/me/tox1que/survivalextender/SurvivalExtender.java
@@ -2,11 +2,18 @@ package me.tox1que.survivalextender;
 
 import me.tox1que.survivalextender.plugins.HalloweenSeason.HalloweenPlugin;
 import me.tox1que.survivalextender.plugins.RecipePlugin.RecipePlugin;
+import me.tox1que.survivalextender.plugins.ThreeWiseMen.ThreeWiseMenPlugin;
 import me.tox1que.survivalextender.utils.Utils;
 import me.tox1que.survivalextender.utils.abstracts.BasePlugin;
+import me.tox1que.survivalextender.utils.enums.ServerType;
+import net.md_5.bungee.api.ChatColor;
+import net.milkbowl.vault.economy.Economy;
 import org.bukkit.Bukkit;
-import org.bukkit.ChatColor;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.potion.PotionEffect;
+import org.bukkit.potion.PotionEffectType;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -14,6 +21,7 @@ import java.util.List;
 public final class SurvivalExtender extends JavaPlugin{
 
     private static SurvivalExtender main;
+    private Economy economy;
 
     private HalloweenPlugin halloweenPlugin;
     private ThreeWiseMenPlugin threeWiseMenPlugin;
@@ -26,6 +34,12 @@ public final class SurvivalExtender extends JavaPlugin{
 
     @Override
     public void onEnable(){
+        if(!setupEconomy()){
+            this.getLogger().info("Missing Vault plugin, disabling SurvivalExtender...");
+            Bukkit.getPluginManager().disablePlugin(this);
+            return;
+        }
+
         main = this;
 
         this.prefix = "§3Systém";
@@ -34,18 +48,25 @@ public final class SurvivalExtender extends JavaPlugin{
 
         this.plugins = new ArrayList<>();
 
+        if(Utils.getServerType() == ServerType.SURVIVAL){
+            specialItemsChecker();
+        }
+
         loadManagers();
     }
 
     @Override
     public void onDisable(){
-        // Plugin shutdown logic
+        plugins.forEach(BasePlugin::unload);
     }
 
     private void loadManagers(){
         new RecipePlugin();
-        if(Utils.getServerName().equals("OldSurvival") || Bukkit.getServer().getPort() == 30015){
-            halloweenPlugin = new HalloweenPlugin();
+//        if(Utils.getServerName().equals("OldSurvival") || Bukkit.getServer().getPort() == 30015){
+//            halloweenPlugin = new HalloweenPlugin();
+//        }
+        if(Utils.getServerType() == ServerType.SURVIVAL){
+//            threeWiseMenPlugin = new ThreeWiseMenPlugin();
         }
 
         plugins.forEach(BasePlugin::load);
@@ -55,16 +76,37 @@ public final class SurvivalExtender extends JavaPlugin{
         plugins.add(plugin);
     }
 
+    public void specialItemsChecker(){
+        Bukkit.getScheduler().scheduleSyncRepeatingTask(this, () -> {
+            Bukkit.getOnlinePlayers().forEach(player -> {
+                ItemStack helmet = player.getInventory().getHelmet();
+                if(helmet != null && helmet.getItemMeta() != null){
+                    if(helmet.getItemMeta().getDisplayName().equals(ChatColor.of("#CCBC29")+"§lBaltazarova korunka")){
+                        player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 200, 1));
+                    }
+                }
+            });
+        }, 100L, 100L);
+    }
+
     public static SurvivalExtender getInstance(){
         return main;
     }
 
-    /*
-        Plugin getters
-     */
+    private boolean setupEconomy(){
+        if(Bukkit.getPluginManager().getPlugin("Vault") == null)
+            return false;
 
-    public HalloweenPlugin getHalloweenPlugin(){
-        return halloweenPlugin;
+        RegisteredServiceProvider<Economy> rsp = this.getServer().getServicesManager().getRegistration(Economy.class);
+        if(rsp == null)
+            return false;
+
+        economy = rsp.getProvider();
+        return true;
+    }
+
+    public Economy getEconomy(){
+        return economy;
     }
 
     public String getPrefix(){
