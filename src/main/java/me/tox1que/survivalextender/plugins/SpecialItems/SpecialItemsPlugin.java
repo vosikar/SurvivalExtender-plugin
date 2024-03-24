36272f1bde6c75b024c7cad578b4def85bac3feb
tox1que <tox1que@minehub.cz>
2024-03-24 19:08:21 +0100

    add "Plavecká čepice" item effect

--- src/main/java/me/tox1que/survivalextender/plugins/SpecialItems/SpecialItemsPlugin.java
+++ src/main/java/me/tox1que/survivalextender/plugins/SpecialItems/SpecialItemsPlugin.java
@@ -20,8 +20,13 @@ import java.util.Map;
 
 public class SpecialItemsPlugin extends BasePlugin{
 
-    private final String MINER_INVENTORY_NAME = ItemUtils.colorize("#00C0CCKopáčův inventář");
-    private final Map<String, Inventory> minerInventories = new HashMap<>();
+    private final String MINER_INVENTORY_NAME;
+    private final Map<String, Inventory> minerInventories;
+
+    public SpecialItemsPlugin(){
+        MINER_INVENTORY_NAME = ItemUtils.colorize("#00C0CCKopáčův inventář");
+        minerInventories = new HashMap<>();
+    }
 
     @Override
     public void load(){
@@ -60,9 +65,13 @@ public class SpecialItemsPlugin extends BasePlugin{
         Bukkit.getScheduler().scheduleSyncRepeatingTask(main, () -> Bukkit.getOnlinePlayers().forEach(player -> {
             ItemStack helmet = player.getInventory().getHelmet();
             if(helmet != null && helmet.getItemMeta() != null){
-                if(helmet.getItemMeta().getDisplayName().equals(ChatColor.of("#CCBC29") + "§lBaltazarova korunka")){
+                String displayName = helmet.getItemMeta().getDisplayName();
+                if(displayName.equals(ChatColor.of("#CCBC29") + "§lBaltazarova korunka")){
                     player.addPotionEffect(new PotionEffect(PotionEffectType.REGENERATION, 200, 1));
+                }else if(displayName.equals(ChatColor.of("#009987")+"§lPlavecká čepice")){
+                    player.addPotionEffect(new PotionEffect(PotionEffectType.DOLPHINS_GRACE, 200, 5));
                 }
+
             }
         }), 1000L, 100L);
     }
@@ -71,7 +80,7 @@ public class SpecialItemsPlugin extends BasePlugin{
         Bukkit.getScheduler().scheduleSyncRepeatingTask(main, () -> Bukkit.getOnlinePlayers().forEach(player -> {
             if(minerInventories.containsKey(player.getName())){
                 if(Arrays.stream(minerInventories.get(player.getName()).getContents()).anyMatch(is -> is != null && is.getType() != Material.AIR)){
-                    sendAlertMessage(player, "Pozor! Máš v Kopačově inventáři uložené itemy, které ti po restartu zmizí.");
+                    sendAlertMessage("Pozor! Máš v Kopačově inventáři uložené itemy, které ti po restartu zmizí.", player);
                 }
             }
         }), 1000L, 20 * 60 * 5L);
